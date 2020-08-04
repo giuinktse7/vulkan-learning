70bbf8752917fdde34b9e566cfc65fe9e78a2def
Jonathan Krän <jonathan.kran1@gmail.com>
2020-08-04 20:11:01 +0200

    Add some more logging utility functions

--- vulkan-tutorial/Logger.h
+++ vulkan-tutorial/Logger.h
@@ -2,12 +2,20 @@
 
 #include <iostream>
 #include <string_view>
-
-constexpr std::string_view InfoString = "[INFO]";
+#include <string>
 
 #define LOG_INFO std::cout << info
 
 namespace Logger
 {
   std::ostream &info();
+  void info(std::string &s);
+
+  std::ostream &debug();
+  void debug(const char *s);
+  void debug(std::string s);
+
+  std::ostream &error();
+  void error(std::string &s);
+  void error(std::string s);
 } // namespace Logger
