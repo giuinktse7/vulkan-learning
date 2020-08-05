2426945e3b877426a8b57cfede4d56502cd2c2b0
Jonathan Krän <jonathan.kran1@gmail.com>
2020-08-05 17:31:07 +0200

    Add first version of ItemAnimator

--- vulkan-tutorial/item_animator.h
+++ vulkan-tutorial/item_animator.h
@@ -2,12 +2,58 @@
 
 #include "graphics/appearances.h"
 
+#include <chrono>
+#include <variant>
+
 class ItemAnimator
 {
 public:
 	ItemAnimator(SpriteAnimation &animation);
 
-	uint32_t getStartPhase() const;
-
 	SpriteAnimation &animation;
+
+	uint32_t getPhase();
+
+	void update();
+
+private:
+	const uint32_t startPhase;
+	/*
+		Used for async animation
+	*/
+	const std::chrono::steady_clock::time_point baseTime;
+
+	enum class Direction
+	{
+		Forward,
+		Backward
+	};
+
+	struct
+	{
+		uint32_t phaseIndex;
+		/*
+			Phase duration in milliseconds.
+		*/
+		uint32_t phaseDurationMs;
+
+		using loop_t = uint32_t;
+		/*
+			Holds necessary information about the animation state based on the
+			animation type.
+			AnimationLoopType::Infinte -> std::monostate
+			AnimationLoopType::PingPong -> ItemAnimator::Direction
+			AnimationLoopType::Counted -> loop_t
+		*/
+		std::variant<std::monostate, loop_t, ItemAnimator::Direction> info;
+		/*
+			The time that the latest phase change occurred.
+		*/
+		std::chrono::steady_clock::time_point lastUpdateTime;
+	} state;
+
+	void setPhase(uint32_t phase);
+
+	void updatePingPong();
+	void updateCounted();
 };
