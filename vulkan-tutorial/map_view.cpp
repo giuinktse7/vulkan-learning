f0c8d63c054709a24240313e468b1c2b7305f3cb
Jonathan Krän <jonathan.kran1@gmail.com>
2020-08-10 21:31:37 +0200

    Add MapView class

---
+++ vulkan-tutorial/map_view.cpp
@@ -0,0 +1,58 @@
+#include "map_view.h"
+
+#include "const.h"
+
+MapView::MapView(GLFWwindow *window) : window(window), map(std::make_shared<Map>())
+{
+}
+
+void MapView::updateViewport()
+{
+  glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
+
+  viewport.zoom = 1 / camera.zoomFactor;
+  viewport.offsetX = camera.position.x;
+  viewport.offsetY = camera.position.y;
+}
+
+void MapView::zoomOut()
+{
+  camera.zoomOut();
+}
+void MapView::zoomIn()
+{
+  camera.zoomIn();
+}
+void MapView::resetZoom()
+{
+  camera.resetZoom();
+}
+
+float MapView::getZoomFactor() const
+{
+  return camera.zoomFactor;
+}
+
+void MapView::translateCamera(glm::vec3 delta)
+{
+  camera.translate(delta);
+}
+void MapView::translateCameraZ(int z)
+{
+  camera.translateZ(z);
+}
+
+Viewport::BoundingRect MapView::getGameBoundingRect() const
+{
+  WorldPosition worldPos{viewport.offsetX, viewport.offsetY};
+  MapPosition mapPos = worldPos.mapPos();
+
+  auto [width, height] = ScreenPosition{static_cast<double>(viewport.width), static_cast<double>(viewport.height)}.mapPos(*this);
+  Viewport::BoundingRect rect;
+  rect.x1 = mapPos.x;
+  rect.y1 = mapPos.y;
+  rect.x2 = mapPos.x + width;
+  rect.y2 = mapPos.y + height;
+
+  return rect;
+}
---
+++ vulkan-tutorial/map_view.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <memory>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "map.h"
+#include "camera.h"
+
+struct Viewport
+{
+	struct BoundingRect
+	{
+		int x1, y1, x2, y2;
+	};
+
+	int width;
+	int height;
+	float zoom;
+
+	uint32_t offsetX;
+	uint32_t offsetY;
+};
+
+class MapView
+{
+public:
+	MapView(GLFWwindow *window);
+	Map *getMap() const
+	{
+		return map.get();
+	}
+
+	void zoomOut();
+	void zoomIn();
+	void resetZoom();
+
+	void updateViewport();
+
+	float getZoomFactor() const;
+
+	const MapPosition worldToMapPos(WorldPosition worldPos) const;
+	const Position screenToMapPos(ScreenPosition screenPos) const;
+	const MapPosition windowToMapPos(ScreenPosition screenPos) const;
+	const uint32_t windowToMapPos(int windowPos) const;
+	const uint32_t mapToWorldPos(uint32_t mapPos) const;
+
+	uint32_t getZ() const
+	{
+		return static_cast<uint32_t>(camera.position.z);
+	}
+
+	/*
+		Synonym for getZ()
+	*/
+	uint32_t getFloor() const
+	{
+		return getZ();
+	}
+
+	uint32_t getX() const
+	{
+		return static_cast<uint32_t>(camera.position.x);
+	}
+	uint32_t getY() const
+	{
+		return static_cast<uint32_t>(camera.position.y);
+	}
+
+	void translateCamera(glm::vec3 delta);
+
+	void translateCameraZ(int z);
+
+	const Viewport &getViewport() const
+	{
+		return viewport;
+	}
+
+	Viewport::BoundingRect getGameBoundingRect() const;
+
+private:
+	GLFWwindow *window;
+	Viewport viewport;
+
+	Camera camera;
+
+	std::shared_ptr<Map> map;
+};
+
+inline std::ostream &operator<<(std::ostream &os, const Viewport::BoundingRect &rect)
+{
+	os << "{ x1=" << rect.x1 << ", y1=" << rect.y1 << ", x2=" << rect.x2 << ", y2=" << rect.y2 << "}";
+	return os;
+}
